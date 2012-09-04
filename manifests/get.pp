c83e8ded4a2b76cdd95db6e9333f27d893dfe7ad
Rafael Luque <rluque>
2012-09-04 11:08:35 +0200

    Added new parameters to s3cmd::get resource

--- manifests/get.pp
+++ manifests/get.pp
@@ -2,10 +2,16 @@ define s3cmd::get(
     $bucket,
     $object,
     $local_file,
-    $options) {
+    $options,
+    $tries = 1,
+    $try_sleep = 1,
+    $config_file = '/root/.s3cfg') {
 
   exec { "s3cmd-get-$bucket-$object":
-    command => "s3cmd $options get s3://$bucket/$object $local_file"
+    command => "s3cmd -c $config_file $options get s3://$bucket/$object $local_file",
+    logoutput => 'on_failure',
+    tries => $tries,
+    try_sleep => $try_sleep
   }
   
 }
